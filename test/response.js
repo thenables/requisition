7df0851f8121b47b9f2aa07619a04b1e1d2f13a5
haoxin <coderhaoxin@outlook.com>
2014-12-04 17:15:44 +0800

    response: add methods charset, etag, lastModified, add decompression for saveTo

--- test/response.js
+++ test/response.js
@@ -1,5 +1,7 @@
 
+var Promise = require('native-or-bluebird');
 var tmpdir = require('os').tmpdir();
+var express = require('express');
 var join = require('path').join;
 var assert = require('assert');
 var fs = require('fs');
@@ -21,6 +23,66 @@ describe('Response', function () {
 
     it('.is(html)', function () {
       assert.equal(res.is('html'), 'html')
+    })
+  })
+
+  describe('.etag()', function () {
+    it('should return etag', function () {
+      var app = express();
+      app.use(function (req, res) {
+        res.set('ETag', 'test-etag');
+        res.end('test');
+      })
+      return new Promise(function (resolve, reject) {
+        app.listen(function (err) {
+          if (err) throw err;
+          resolve(this.address().port);
+        })
+      }).then(function (port) {
+        return request('http://127.0.0.1:' + port).then(function (response) {
+          assert(response.etag(), 'test-etag');
+        })
+      })
+    })
+  })
+
+  describe('.charset()', function () {
+    it('should return charset', function () {
+      var app = express();
+      app.use(function (req, res) {
+        res.send('test');
+      })
+      return new Promise(function (resolve, reject) {
+        app.listen(function (err) {
+          if (err) throw err;
+          resolve(this.address().port);
+        })
+      }).then(function (port) {
+        return request('http://127.0.0.1:' + port).then(function (response) {
+          assert.equal(response.charset(), 'utf-8');
+        })
+      })
+    })
+  })
+
+  describe('.lastModified()', function () {
+    it('should return lastModified', function () {
+      var app = express();
+      var lastModified = new Date(1417681633452).toUTCString();
+      app.use(function (req, res) {
+        res.set('Last-Modified', lastModified);
+        res.end('test');
+      })
+      return new Promise(function (resolve, reject) {
+        app.listen(function (err) {
+          if (err) throw err;
+          resolve(this.address().port);
+        })
+      }).then(function (port) {
+        return request('http://127.0.0.1:' + port).then(function (response) {
+          assert(response.lastModified(), lastModified);
+        })
+      })
     })
   })
 
@@ -72,11 +134,22 @@ describe('Response', function () {
   })
 
   describe('.saveTo()', function () {
-    var filename = join(tmpdir, Math.random().toString(36).slice(2));
-    return request('http://ip.jsontest.com/').then(function (response) {
-      return response.saveTo(filename);
-    }).then(function () {
-      return fs.statSync(filename);
+    it('should save to file', function () {
+      var filename = join(tmpdir, Math.random().toString(36).slice(2));
+      return request('http://ip.jsontest.com/').then(function (response) {
+        return response.saveTo(filename);
+      }).then(function () {
+        return fs.statSync(filename);
+      })
+    })
+
+    it('should decompress and save to file', function () {
+      var filename = join(tmpdir, Math.random().toString(36).slice(2));
+      return request('https://github.com').then(function (response) {
+        return response.saveTo(filename);
+      }).then(function () {
+        assert.equal(fs.readFileSync(filename, 'utf8').trim().indexOf('<!DOCTYPE html>'), 0);
+      })
     })
   })
 })
